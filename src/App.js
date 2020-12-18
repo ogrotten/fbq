1a73531368d20055e9e7d68128197ca5488c5df1
chip <chiposaur@gmail.com>
2020-12-18 15:42:53 -0600

    breakpoints

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React from 'react'
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+import {Block} from 'baseui/block'
+import { StatefulInput } from 'baseui/input';
+
+import Nav from "./Nav"
 
 import './App.css';
 
-function App() {
+const engine = new Styletron();
+const Centered = styled('div', {
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center',
+	height: '100%',
+});
+
+const App = () => {
 	return (
-		<div className="container">
-			<nav>navbar</nav>
-			<aside>sidebar</aside>
-			<section>main</section>
-			<footer>footer</footer>
-		</div>
+		<StyletronProvider value={engine}>
+			<BaseProvider theme={LightTheme}>
+				<Block as="div" className="container">
+					<nav><Nav /></nav>
+					{/* <Nav /> */}
+					<aside></aside>
+					<section></section>
+					<footer></footer>
+				</Block>
+			</BaseProvider>
+		</StyletronProvider>
 	);
 }
 
